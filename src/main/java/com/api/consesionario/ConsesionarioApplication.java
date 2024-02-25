54466c06844fb9db499e073853e8a30bf8536729
Santiagop212 <thiagoposada254@gmail.com>
2024-02-25 00:10:58 -0500

    rama de desarrollo o de pruebas

--- src/main/java/com/api/consesionario/ConsesionarioApplication.java
+++ src/main/java/com/api/consesionario/ConsesionarioApplication.java
@@ -3,6 +3,8 @@ package com.api.consesionario;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+//AMBIENTE DE PRUEBAS
 //consesionario = concesionario
 @SpringBootApplication
 public class ConsesionarioApplication {
